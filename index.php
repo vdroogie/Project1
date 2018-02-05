6a86300ab3188c9dcc5bd543342823e2488b4c12
vdroogie <victor03droogie@gmail.com>
2018-02-05 10:52:17 +0100

    Segundo commit

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ and open the template in the editor.
     <body>
         <?php
         echo "Primera linea del proyecto";
+        $hola="Segunda linea del proyecto";
+        echo $hola;
         ?>
     </body>
 </html>
